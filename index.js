9537639b61656c111fc7c75baee0659922f14cd2
Michael Fuget <michaelfuget@gmail.com>
2021-02-02 01:47:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,5 +31,5 @@ function greet(time)
 /* Write your implementation of displayMessage() */
 function displayMessage(inputText)
 {
-  document.getElementById('greeting').text = inputText
+  document.getElementById('greeting').textContent = inputText
 }
